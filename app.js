0f0c92856c44de202be62c316fd086e339c2b7a1
Daniel Leal <daniel.leal@my.scitexas.edu>
2017-09-11 04:17:29 +0000

    Added javascipt into html as a script tag instead of a seperate app.js file

--- app.js
+++ app.js
@@ -1,52 +1,67 @@
-var input = document.getElementById('input');
-var grade = document.getElementsByClassName('grade');
+// var input = document.getElementById('input');
+// var grade = document.getElementsByClassName('grade');
 
 
 
-var span = "";
-var gradeArray = [];
-var average = 0;
-var sumArr = 0;
+// var span = "";
+// var gradeArray = [];
+// var average = 0;
+// var sumArr = 0;
 
 
 
-function calculateGrade(){
+// function calculateGrade(){
     
-    var gradeValue = document.getElementById('input').value;
-    var inputGrade = input.value;
+//     var gradeValue = document.getElementById('input').value;
+//     var inputGrade = input.value;
     
-    if(inputGrade <= 100){
-        span = "A";
-    }
+//     if(inputGrade <= 100){
+//         span = "A";
+//     }
     
-    if(inputGrade <= 89){
-        span = "B";
-    }
+//     if(inputGrade <= 89){
+//         span = "B";
+//     }
     
-    if(inputGrade <= 79){
-        span ="C";
-    }
+//     if(inputGrade <= 79){
+//         span ="C";
+//     }
     
-    if(inputGrade <=69){
-        span ="D";
-    }
+//     if(inputGrade <=69){
+//         span ="D";
+//     }
     
-    if(inputGrade <=59){
-        span ="F";
-    }
-    
-    
-    document.getElementById('grade-letter').innerHTML = span;
-    document.getElementById('average').innerHTML = average; 
-    
-    document.getElementById('input').value = '';
+//     if(inputGrade <=59){
+//         span ="F";
+//     }
     
     
+//     document.getElementById('grade-letter').innerHTML = span;
+//     document.getElementById('average').innerHTML = average; 
     
-    for (i = 0; i < gradeArray.length; i++) {
-      sumArr += +gradeArray[i];
-      console.log(sumArr);
-   }
+//     document.getElementById('input').value = '';
     
     
-}
+    
+//     for (i = 0; i < gradeArray.length; i++) {
+//       sumArr += +gradeArray[i];
+//       console.log(sumArr);
+//   }
+   
+//   if (inputGrade > 100 || inputGrade == '') {
+//       alert("Invalid Grade Value");
+//       gradeArray.slice();
+//   }
+   
+//     else {
+//       gradeArray.push(inputGrade);
+//   }
+
+//   // Divides sum of array then divides by the amount of numbers in array
+//   average = sumArr / gradeArray.length;
+   
+//   // Console.log for debugging
+//   console.log(gradeArray);
+    
+    
+// }
